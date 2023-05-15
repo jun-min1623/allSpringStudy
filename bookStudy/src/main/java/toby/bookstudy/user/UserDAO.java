8e1db0b2513a7940e5bb06ca8e31269e30926f6b
jun-min1623 <junmin1623@naver.com>
2023-05-15 09:04:30 +0900

    장실커밋

--- bookStudy/src/main/java/toby/bookstudy/user/UserDAO.java
+++ bookStudy/src/main/java/toby/bookstudy/user/UserDAO.java
@@ -1,4 +1,20 @@
 package toby.bookstudy.user;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserDAO {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/springbook", "spring", "book");
+        PreparedStatement ps = c.prepareStatement("");
+
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
 }
--- bookStudy/src/main/java/toby/bookstudy/user/UserDaoRepo.java
+++ bookStudy/src/main/java/toby/bookstudy/user/UserDaoRepo.java
@@ -1,2 +1,42 @@
-package toby.bookstudy.user;public class UserDaoRepo {
+package toby.bookstudy.user;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDaoRepo {
+    public void deleteAll() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            DataSource dataSource = null;
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                }catch (SQLException e){
+
+                }
+            }
+            if (c == null) {
+                try {
+                    c.close();
+                }catch (SQLException e){
+
+                }
+            }
+        }
+
+    }
+
+
 }
