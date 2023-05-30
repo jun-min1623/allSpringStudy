297b503d2214f5fd2a9a8fa8c19f2808499af05d
jun-min1623 <junmin1623@naver.com>
2023-05-30 13:25:29 +0900

    java 스터디용 공간 생성

---
+++ javaStudyWithSpring/src/main/java/java/study/javastudywithspring/JavaStudyWithSpringApplication.java
@@ -0,0 +1,13 @@
+package java.study.javastudywithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStudyWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStudyWithSpringApplication.class, args);
+	}
+
+}
---
+++ javaStudyWithSpring/src/test/java/java/study/javastudywithspring/JavaStudyWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package java.study.javastudywithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
